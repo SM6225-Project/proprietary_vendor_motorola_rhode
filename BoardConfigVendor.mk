c63c4da355c91f77c4812e50abfa76305271f02e
josexda <josegustavo6@protonmail.com>
2024-11-11 19:22:10 -0300

    Revert "rhode: Import firmware from T2SR33.72-22-4"
    This reverts commit 0da3a5ab33a7beb96a3c8b6c2b3ba74531217e6b.
    
    Change-Id: Ic944a588e895c06a44f80557699ff967c9f72213

--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -2,21 +2,3 @@
 # Automatically generated file. DO NOT MODIFY
 #
 
-AB_OTA_PARTITIONS += \
-    abl \
-    bluetooth \
-    devcfg \
-    dsp \
-    fsg \
-    hyp \
-    keymaster \
-    logo \
-    modem \
-    prov \
-    qupfw \
-    rpm \
-    storsec \
-    tz \
-    uefisecapp \
-    xbl \
-    xbl_config
